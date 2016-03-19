eb02214abeced8a4ac73677ef2db81a40abba965
matsumatsu20 <matsumatsu20@gmail.com>
2016-03-19 23:44:07 +0900

    16をといた

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 
 ## Stats for execution
 
-| Puzzle No. | Average    | Max       | Min    |
-| :--------: | :-----:    | :-:       | :-:    |
-| Question3  | 0.064ms    | 0.433ms   | 0.02ms |
-| Question6  | 6.052ms    | 60.23ms   | 0.02ms |
+| Puzzle No. | Average    | Max       | Min      |
+| :--------: | :-----:    | :-:       | :-:      |
+| Question3  | 0.2668ms   | 1.106ms   | 0.132ms  |
+| Question6  | 56.7152ms  | 61.161ms  | 54.178ms |
+| Question6  | 3.7011ms   | 4.724ms   | 3.455ms  |
---
+++ src/questions/16.rb
@@ -0,0 +1,46 @@
+# $answer = []
+$stored_ratio = []
+
+class Question16
+  def main
+    (4..500).step(4) do |i|
+      calculate(i)
+    end
+  end
+
+  def calculate(i)
+    n = 1
+    square_list = []
+    square_area = i / 4 * i / 4
+
+    while n < i / 4
+      square_list << n * (i / 2 - n) if square?(n * (i / 2 - n))
+      n += 1
+    end
+
+    unless square_list.empty?
+      square_list.each do |pp|
+        square_list.reverse.each do |ff|
+          if square_area == pp + ff
+            ratio = Math.sqrt(ff).to_f / Math.sqrt(pp).to_f
+            unless $stored_ratio.include?(ratio)
+              # $answer << square_area.to_s + ' =  + ' + pp.to_s + '+' + ff.to_s
+              $stored_ratio << ratio
+            end
+          end
+        end
+        square_list.delete(pp)
+      end
+    end
+  end
+
+  def square?(i)
+    Math.sqrt(i) % 1 == 0.0
+  end
+end
+
+q16 = Question16.new
+q16.main
+
+# $answer.each{|ans| p ans}
+p $stored_ratio.size
